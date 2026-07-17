10a059dd0d069711f7148c0b0d47cd4b267ddd46
Stas Zotov <zss1988@ya.ru>
2026-07-17 23:47:57 +0300

    Ни как с архитектурой не решу

--- cmd/pcs/main.go
+++ cmd/pcs/main.go
@@ -16,14 +16,7 @@ func (s *svc) GetConfig(info map[string]string) ([]byte, error) {
 func main() {
 	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	log.Info("Server started")
-	repo := struct {
-		db *sql.DB
-	}{
-		db: &sql.DB{},
-	}
-	db := *sql.DB
 
-}
 
 // http.ListenAndServe(":8080", router.Handler())
 // fmt.Println("Server listen on port :8080")
